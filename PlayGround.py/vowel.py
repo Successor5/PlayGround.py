5e425e94bed55e2593024e30a62f96988a653ef9
Debbie <119403063+debby71711@users.noreply.github.com>
2023-05-01 16:14:26 +0100

    Add files via upload

---
+++ PlayGround.py/vowel.py
@@ -0,0 +1,4 @@
+class vowel:
+    word=str(input("enter a sentence"))
+    vowel=1
+    consonant=1
---
+++ PlayGround.py/wordsCount.py
@@ -0,0 +1,9 @@
+class word_count:
+    me = str (input("enter a string"))
+    count = 0
+
+    for i in me:
+        count+=1
+
+    print(count)
+
